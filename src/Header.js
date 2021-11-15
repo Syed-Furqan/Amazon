c2ea7ea7af0c9874b137b45bfb20755726698e66
Syed Furqan <syedfurqan2001@gmail.com>
2021-11-15 16:22:37 +0530

    Header styling complete

--- src/Header.js
+++ src/Header.js
@@ -1,42 +1,22 @@
-import "./Header.css"
-import SearchIcon from "@material-ui/icons/Search"
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+import HeaderLarge from "./HeaderLarge";
+import HeaderSmall from "./HeaderSmall";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <img
-                className="header__logo" 
-                src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
-                alt="amazon-logo"
-            />
-            <div className="header__search">
-                <input className="header__searchInput" type="text"/>
-                < SearchIcon className="header__searchIcon"/>
-            </div>
 
-            <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLine1">Hello Guest</span>
-                    <span className="header__optionLine2">Sign In</span>
-                </div>
-                <div className="header__option">
-                    <span className="header__optionLine1">Returns</span>
-                    <span className="header__optionLine2">& Orders</span>
-                </div>
-                <div className="header__option">
-                    <span className="header__optionLine1">Your</span>
-                    <span className="header__optionLine2">Prime</span>
-                </div>
-            </div>
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 620;
+  
+    useEffect(() => {
+      const handleWindowResize = () => setWidth(window.innerWidth)
+      window.addEventListener("resize", handleWindowResize);
+  
+      // Return a function from the effect that removes the event listener
+      return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+  
+    return width < breakpoint ? < HeaderSmall /> : <HeaderLarge />;
 
-            <div className="header__optionBasket">
-                < ShoppingBasketIcon />
-                <span className="header__optionLine2 header__basketCount">0</span>
-            </div>
-            
-        </div>
-    );
 }
 
 export default Header;
---
+++ src/HeaderSmall.js
@@ -0,0 +1,9 @@
+import "./HeaderSmall.css"
+
+const HeaderSmall = () => {
+    return (
+        <h1>Small Devices Header</h1>
+    );
+}
+
+export default HeaderSmall;
