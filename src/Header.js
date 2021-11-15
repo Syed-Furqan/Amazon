7988544b851a3c25860fcadc336327174f7b5e39
Syed Furqan <syedfurqan2001@gmail.com>
2021-11-15 15:33:44 +0530

    Navbar styling done

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import "./Header.css"
+import SearchIcon from "@material-ui/icons/Search"
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img
+                className="header__logo" 
+                src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
+                alt="amazon-logo"
+            />
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+                < SearchIcon className="header__searchIcon"/>
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLine1">Hello Guest</span>
+                    <span className="header__optionLine2">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLine1">Returns</span>
+                    <span className="header__optionLine2">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLine1">Your</span>
+                    <span className="header__optionLine2">Prime</span>
+                </div>
+            </div>
+
+            <div className="header__optionBasket">
+                < ShoppingBasketIcon />
+                <span className="header__optionLine2 header__basketCount">0</span>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Header;
